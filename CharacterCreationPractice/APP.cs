52d25fd6b3fe6f4ff1707d39f398af95f919542d
Christian Roberts <christian.m.roberts@outlook.com>
2020-10-26 07:37:34 -0400

    Injected some prerequisites to hit the ground running

---
+++ CharacterCreationPractice/APP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMenuProject
+{
+    class APP
+    {
+        public void Run()
+        {
+            Console.WriteLine("Yo from the APP");
+            new Main_Menu().Run();
+            Console.Clear();
+            Console.WriteLine("Yo from the APP");
+        }
+    }
+}
--- CharacterCreationPractice/Program.cs
+++ CharacterCreationPractice/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace CharacterCreationPractice
+namespace MainMenuProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new APP().Run();
             Console.WriteLine("Hello World!");
         }
     }
 }
+/*Matt M02/13/2020
+And if you cant read my chicken scratch:
+git checkout -b <newbranchname>
+git push --set-upstream origin branchname
+-----DO SOME STUFF-----
+git add .
+git commit -m "write the Did stuff (changes)"
+git push
+git checkout master
+git merge branchname
+git push
+git pull
+git branch -d branchname*/
